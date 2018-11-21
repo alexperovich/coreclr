458c7a16bcda82d70fccf95ef5f8d0614569143f
Egor Chesakov <Egor.Chesakov@microsoft.com>
2018-11-20 17:31:12 -0800

    Add basic support for scenarios in tests\runtest_helix.py

--- tests/runtest_helix.py
+++ tests/runtest_helix.py
@@ -15,12 +15,6 @@ import argparse
 import subprocess
 import os
 import sys
-import tempfile
-
-test_scenarios = {
-    "jitstress2": { "COMPlus_TieredCompilation": "0",
-                    "COMPlus_JitStress": "2" },
-}
 
 if sys.platform == "linux" or sys.platform == "darwin":
     platform_type = "unix"
@@ -30,19 +24,145 @@ else:
     print("unknown os: %s" % sys.platform)
     sys.exit(1)
 
-def get_testenv_script(env_dict):
-    if platform_type == "unix":
-        return ''.join([ "export %s=%s%s" % (k, v, os.linesep) for k, v in env_dict.items() ])
-    elif platform_type == "windows":
-        return ''.join([ "set %s=%s%s" % (k, v, os.linesep) for k, v in env_dict.items() ])
+class ScenarioRegistry:
+    def __init__(self):
+        self._runners = dict()
+
+    def is_known_scenario_name(self, name):
+        return name in self._runners
+
+    def get_scenario_runner(self, name):
+        return self._runners[name]
+
+    def register_scenario_runner(self, name, runner):
+        assert not self.is_known_scenario_name(name)
+        self._runners[name] = runner
+
+class ScenarioRunner:
+    def __init__(self, name, prototype=None):
+        self._name = name
+        self._Core_Root = None
+        self._TestEnv = dict() if prototype is None else dict(prototype._TestEnv)
+
+    def set_COMPlus(self, name, value):
+        self._TestEnv["COMPlus_" + name] = value
+        return self
+
+    def get_Core_Root(self):
+        assert not self._Core_Root is None
+        return self._Core_Root
+
+    def set_Core_Root(self, dirpath):
+        assert os.path.isdir(dirpath)
+        self._Core_Root = dirpath
+
+    def _get_corerun(self):
+        return os.path.join(self.get_Core_Root(), "corerun" if platform_type == "unix" else "CoreRun.exe")
+
+    def _get_console_runner(self):
+        return os.path.join(self.get_Core_Root(), "xunit.console.dll")
+
+    def _create_TestEnv(self, filepath):
+        lines = []
+        for name, value in self._TestEnv.items():
+            lines.append("{0} {1}={2}\n".format("set" if platform_type == "windows" else "export", name, value))
+
+        with open(filepath, "w") as f:
+            f.writelines(lines)
+
+    def _log(self, message):
+        print("[%s]: %s" % (sys.argv[0], message))
+
+    def register(self, registry):
+        registry.register_scenario_runner(self._name, self)
+
+    def run(self, test_wrapper_filepath):
+        assert os.path.isfile(test_wrapper_filepath)
+
+        args = [
+            self._get_corerun(),
+            self._get_console_runner(),
+            test_wrapper_filepath,
+            "-nocolor",
+            "-noshadow",
+            "-xml", "testResults.xml",
+            "-notrait", "category=outerloop",
+            "-notrait", "category=failing",
+            "-parallel", "collections"]
+
+        environ = dict(os.environ)
+        environ["CORE_ROOT"] = self.get_Core_Root()
+        self._log("CORE_ROOT=%s" % self.get_Core_Root())
+
+        env_script_filepath = os.path.join(os.getcwd(), "SetStressModes.bat")
+        environ["__TestEnv"] = env_script_filepath
+
+        self._log("Creating __TestEnv at {0}".format(env_script_filepath))
+        self._create_TestEnv(env_script_filepath)
+
+        self._log("BEGIN EXECUTION")
+        self._log(" ".join(args))
+
+        proc = subprocess.Popen(args, env=environ)
+        proc.communicate()
+
+        self._log("Finished running tests. Exit code = %d" % proc.returncode)
+        return proc.returncode
+
+def build_registry():
+    registry = ScenarioRegistry()
+
+    baseline = ScenarioRunner("baseline") \
+        .set_COMPlus("TieredCompilation", "0")
+
+    baseline.register(registry)
+
+    # Jit Stress Scenarios
+
+    ScenarioRunner("jitstress1", baseline) \
+        .set_COMPlus("JitStress", "1") \
+        .register(registry)
+
+    ScenarioRunner("jitstress2", baseline) \
+        .set_COMPlus("JitStress", "2") \
+        .register(registry)
+
+    ScenarioRunner("jitstress1_tiered") \
+        .set_COMPlus("JitStress", "2") \
+        .set_COMPlus("TieredCompilation", "1") \
+        .register(registry)
+
+    ScenarioRunner("jitstress2_tiered") \
+        .set_COMPlus("JitStress", "2") \
+        .set_COMPlus("TieredCompilation", "1") \
+        .register(registry)
+
+    # GC Stress Scenarios
+
+    ScenarioRunner("gcstress0x3", baseline) \
+        .set_COMPlus("COMPlus_GCStress", "0x3") \
+        .register(registry)
+
+    ScenarioRunner("gcstress0xc", baseline) \
+        .set_COMPlus("COMPlus_GCStress", "0xc") \
+        .register(registry)
+
+    return registry
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Parse arguments")
-    parser.add_argument("-scenario", dest="scenario", default=None)
-    parser.add_argument("-wrapper", dest="wrapper", default=None, required=True)
+    parser.add_argument("-scenario", dest="scenario", default="baseline")
+    parser.add_argument("-wrapper", dest="wrapper", required=True)
+
     args = parser.parse_args()
+
     scenario = args.scenario
-    wrapper = args.wrapper
+
+    registry = build_registry()
+
+    if not registry.is_known_scenario_name(scenario):
+        print("Scenario \"%s\" is unknown" % scenario)
+        sys.exit(1)
 
     if not "HELIX_CORRELATION_PAYLOAD" in os.environ:
         print("HELIX_CORRELATION_PAYLOAD must be defined in environment")
@@ -52,53 +172,10 @@ if __name__ == "__main__":
         print("HELIX_WORKITEM_PAYLOAD must be defined in environment")
         sys.exit(1)
 
-    core_root = os.environ["HELIX_CORRELATION_PAYLOAD"]
+    test_wrapper_filepath = os.path.join(os.environ["HELIX_WORKITEM_PAYLOAD"], args.wrapper)
 
-    if platform_type == "unix":
-        corerun = os.path.join(core_root, "corerun")
-    else:
-        corerun = os.path.join(core_root, "corerun.exe")
+    runner = registry.get_scenario_runner(scenario)
+    runner.set_Core_Root(os.environ["HELIX_CORRELATION_PAYLOAD"])
 
-    # Unlike the old test wrapper, this runs xunit.console.dll from
-    # the correlation payload. This removes the need for redundant
-    # copies of the console runner in each test directory.
-    command = [corerun,
-               os.path.join(os.environ["HELIX_CORRELATION_PAYLOAD"], "xunit.console.dll"),
-               os.path.join(os.environ["HELIX_WORKITEM_PAYLOAD"], wrapper),
-               "-noshadow",
-               "-xml", "testResults.xml",
-               "-notrait", "category=outerloop",
-               "-notrait", "category=failing"]
-
-    if scenario is None:
-        print("CORE_ROOT=%s" % core_root)
-        os.environ["CORE_ROOT"] = core_root
-
-        print("BEGIN EXECUTION")
-        print(' '.join(command))
-        proc = subprocess.Popen(command)
-        proc.communicate()
-        print("Finished running tests. Exit code = %d" % proc.returncode)
-        sys.exit(proc.returncode)
-    else:
-        print("scenario: %s" % scenario)
-        with tempfile.NamedTemporaryFile(mode="w") as testenv:
-            testenv.write(get_testenv_script(test_scenarios[scenario]))
-            testenv.flush()
-
-            print("__TestEnv=%s" % testenv.name)
-            os.environ["__TestEnv"] = testenv.name
-
-            with open(testenv.name) as testenv_written:
-                contents = testenv_written.read()
-                print(contents)
-
-            print("CORE_ROOT=%s" % core_root)
-            os.environ["CORE_ROOT"] = core_root
-
-            print("BEGIN EXECUTION")
-            print(' '.join(command))
-            proc = subprocess.Popen(command)
-            proc.communicate()
-            print("Finished running tests. Exit code = %d" % proc.returncode)
-            sys.exit(proc.returncode)
+    returncode = runner.run(test_wrapper_filepath)
+    sys.exit(returncode)
